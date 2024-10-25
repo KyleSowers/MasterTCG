52ba8cc3c023c08d65efd80c364927d6984b489d
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-25 11:29:30 -0400

    Testing JSON script for card

---
+++ JS/LorcanaDB.js
@@ -0,0 +1,18 @@
+//Lorcana Card JSON Database
+
+var LorcanaDB = {
+    "firstChapter": [
+        {
+            "number": "001",
+            "name": "Arial",
+            "subname": "On Human Legs",
+            "classification": {"storyborn", "hero", "princess"},
+            "cost": "4",
+            "strength": "3",
+            "willpower": "4",
+            "loreValue": "2",
+            "artist": "Matthew Robert Davies",
+            "description": "VOICELESS: This character can't '' to sing songs"
+        }
+    ]
+}
