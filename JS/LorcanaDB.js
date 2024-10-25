d6d59f1f1c5dfb92d7eca44b6d813326346067d4
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-25 13:57:23 -0400

    Adds the next card JSON info for testing

--- JS/LorcanaDB.js
+++ JS/LorcanaDB.js
@@ -4,15 +4,40 @@ var LorcanaDB = {
     "firstChapter": [
         {
             "number": "001",
-            "name": "Arial",
+            "name": "Ariel",
             "subname": "On Human Legs",
-            "classification": {"storyborn", "hero", "princess"},
+            "classification":
+                {
+                    "storyborn",
+                    "hero",
+                    "princess"
+                },
             "cost": "4",
             "strength": "3",
             "willpower": "4",
             "loreValue": "2",
             "artist": "Matthew Robert Davies",
             "description": "VOICELESS: This character can't '' to sing songs"
+        },
+        {
+            "number": "002",
+            "name": "Ariel",
+            "subname": "Spectacular Singer",
+            "classification":
+                {
+                    "storyborn",
+                    "hero",
+                    "princess"
+                },
+            "cost": "3",
+            "strength": "2",
+            "willpower": "3",
+            "loreValue": "1",
+            "artist": "Alice Pisoni",
+            "description": "MUSICAL DEBUT: When you play this character, look at the top 4 cards of your deck. You may reveal a song card and put it into your hand. Put the rest on the bottom of your deck in any order."
+        },
+        {
+
         }
     ]
 }
