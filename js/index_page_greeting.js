766abdddacf3403783b3e7ce0f15c2c6f74b741f
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-11-22 21:46:25 -0500

    Adds notes explaining the script for other developers

--- js/index_page_greeting.js
+++ js/index_page_greeting.js
@@ -1,7 +1,10 @@
-var today = new Date();
-var hourNow = today.getHours();
+// This script  displays a greeting to the user based on the current time.
+
+var today = new Date();                 //Create new date object
+var hourNow = today.getHours();       //Find current hour
 var greeting;
 
+// Display the  appropriate greeting based on the current time
 if (hourNow > 18) {
     greeting = 'Good Evening Collector!';
 } else if (hourNow > 12) {
